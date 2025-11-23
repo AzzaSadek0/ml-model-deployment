4ab601e8882ee64cefe2cb236062afecaffec991
AzzaSadek0 <azasadek7@gmail.com>
2025-11-23 04:20:24 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,18 @@
 from fastapi import FastAPI
-import pickle
+from joblib import load
 import pandas as pd
 
 app = FastAPI()
 
-# Load the model
-with open("model.pkl", "rb") as f:
-    model = pickle.load(f)
+# Load model
+model = load("model.joblib")
 
 @app.get("/")
 def root():
-    return {"message": "ML Model API is running successfully!"}
+    return {"message": "API is running"}
 
 @app.post("/predict")
-def predict(input_data: dict):
-    # Example input: { "TV": 200, "Radio": 30, "Newspaper": 45 }
-    df = pd.DataFrame([input_data])
-    prediction = model.predict(df)[0]
-    return {"prediction": float(prediction)}
+def predict(data: dict):
+    df = pd.DataFrame([data])
+    pred = model.predict(df)[0]
+    return {"prediction": float(pred)}
